d4d823b725799e890960ba3f3bca30e5271c1b56
sherewale <sherwale@mail.gvsu.edu>
2022-03-01 18:53:27 -0500

    Initial Commit: Basic connect 4, Diagonal does not work.

--- src/main/java/CreateGrid.java
+++ src/main/java/CreateGrid.java
@@ -10,24 +10,24 @@ import java.awt.Graphics2D;
 
 public class CreateGrid extends JPanel implements MouseListener {
     /** Which player's turn it is.*/
-    private int currentTurn = 1;
+    public static int currentTurn = 1;
     /** The number of rows for the grid.*/
-    private int rows = 6;
+    public static int rows = 6;
     /** The number of columns for the grid.*/
-    private int columns = 7;
+    public static int columns = 7;
     /** Creates a grid of colors for the game.*/
-    private Color[][] cGrid = new Color[rows][columns];
+    public static Color[][] cGrid = new Color[rows][columns];
     /** The starting x position on the GUI.*/
-    private int startingX = 0;
+    public static int startingX = 0;
     /** The starting y position on the GUI.*/
-    private int startingY = 0;
+    public static int startingY = 0;
     /** The width of the pieces and grid spots.*/
-    private int circleWidth = 50;
+    public static int circleWidth = 50;
     /** Which player won the game.*/
-    private int won = 2;
+    public static int won = 2;
     /** int to check for if what is
      * clicked is out of bounds.*/
-    private int outOfBounds = 20;
+    public static int outOfBounds = 20;
     /**
      *
      * Creates a 6 by 7 grid of connect 4.
@@ -92,7 +92,7 @@ public class CreateGrid extends JPanel implements MouseListener {
      */
     public void mousePressed(final MouseEvent e) {
         int xLocation = e.getX() / circleWidth;
-        int yLocation = opening(xLocation);
+        int yLocation = ConnectLogic.opening(xLocation);
         //if the game is ongoing it will update the grid with who's piece it is
         //based on where the board is pressed.
         if (yLocation == outOfBounds) {
@@ -109,101 +109,18 @@ public class CreateGrid extends JPanel implements MouseListener {
         }
         //checks if there is a winner
         //and sets the winner to who won if there is.
-        int result = checkVertical();
-        int result2 = checkHorizontal();
-        if (result == 0 || result2 == 0) {
+        int result = ConnectLogic.checkVertical();
+        int result2 = ConnectLogic.checkHorizontal();
+        int result3 = ConnectLogic.checkDiagonal1();
+        int result4 = ConnectLogic.checkDiagonal2();
+        if (result == 0 || result2 == 0 || result3 == 0 || result4 == 0) {
             won = 0;
-        } else if (result == 1 || result2 == 1) {
+        } else if (result == 1 || result2 == 1
+                || result3 == 1 || result4 == 1) {
             won = 1;
         } else {
             won = 2;
         }
-    }
-
-    /**
-     * Checks for an open spot below where the player pressed.
-     * @param xLocation the x location
-     *                  of the GUI which the player pressed.
-     * @return returns the y location if okay, and outOfbounds if not.
-     */
-    public int opening(final int xLocation) {
-        int yLocation = rows - 1;
-        //checks if the spot in the gird is open
-        // and returns the lowest open spot if available.
-        while (!cGrid[yLocation][xLocation].equals(new Color(255, 255, 255))) {
-            yLocation = yLocation - 1;
-            if (yLocation < 0) {
-                return outOfBounds;
-            }
-        }
-        return yLocation;
-    }
-
-    /**
-     * Checks the vertical win condition of 4 pieces in a row.
-     * @return returns which player won or if neither did.
-     */
-    public int checkVertical() {
-        int check = 0;
-        int check2 = 0;
-        //Goes through the grid and checks for winner vertically.
-        for (int i = 0; i < cGrid[0].length; ++i) {
-            for (int j = 0; j < cGrid[0].length - 1; ++j) {
-                //if there is 4 reds in a row vertically then red won.
-                if (cGrid[j][i].equals(new Color(255, 0, 0))) {
-                    check++;
-                    check2 = 0;
-                    if (check >= 4) {
-                        return 0;
-                    }
-                } else if (cGrid[j][i].equals(new Color(255, 255, 0))) {
-                    //if there is 4 yellows in a row vertically then yellow won.
-                    check = 0;
-                    check2++;
-                    if (check2 >= 4) {
-                        return 1;
-                    }
-                } else {
-                    check = 0;
-                    check2 = 0;
-                }
-            }
-            //return 2 noting that there is no winner and the game is ongoing.
-        }   return 2;
-    }
-
-    /**
-     * Checks the horizontal win condition of 4 pieces in a row.
-     * @return returns which player won or if neither did.
-     */
-    public int checkHorizontal() {
-        int check = 0;
-        int check2 = 0;
-        //Goes through the grid and checks for winner horizontally.
-        for (int i = 0; i < cGrid[0].length - 1; ++i) {
-            for (int j = 0; j < cGrid[0].length; ++j) {
-                //if there is 4 reds in a row horizontally then red won.
-                if (cGrid[i][j].equals(new Color(255, 0, 0))) {
-                    check++;
-                    check2 = 0;
-                    if (check >= 4) {
-                        return 0;
-                    }
-                } else if (cGrid[i][j].equals(new Color(255, 255, 0))) {
-                    //if there is 4 yellows in a
-                    // row horizontally then yellow won.
-                    check = 0;
-                    check2++;
-                    if (check2 >= 4) {
-                        return 1;
-                    }
-                } else {
-                    check = 0;
-                    check2 = 0;
-                }
-            }
-            //return 2 noting that there is no winner and the game is ongoing.
-        }   return 2;
     }
 
     //below are needed for compiling.
